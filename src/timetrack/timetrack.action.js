db1dc083ebb48f40973037773e8905e1410da0f3
Sebastian Springer <info@sebastian-springer.com>
2018-01-28 18:09:25 +0100

    add timetrack

--- src/timetrack/timetrack.action.js
+++ src/timetrack/timetrack.action.js
@@ -1,8 +1,24 @@
 export const LOAD_TIMETRACK = 'LOAD_TIMETRACK';
+export const ADD_TIMETRACK = 'ADD_TIMETRACK';
+export const ADD_TIMETRACK_SUCCESS = 'ADD_TIMETRACK_SUCCESS';
 
 export const loadTimetrack = timetracks => ({
   type: LOAD_TIMETRACK,
   payload: {
     timetracks,
+  },
+});
+
+export const addTimetrack = timetrack => ({
+  type: ADD_TIMETRACK,
+  payload: {
+    timetrack,
+  },
+});
+
+export const addTimetrackSuccess = timetrack => ({
+  type: ADD_TIMETRACK_SUCCESS,
+  payload: {
+    timetrack,
   },
 });
--- src/timetrack/timetrack.epic.js
+++ src/timetrack/timetrack.epic.js
@@ -1,1 +1,13 @@
-export const timetrackEpic = action$ => action$.ofType('dummy').pipe();
+import { mergeMap } from 'rxjs/operator/mergeMap';
+import { Observable } from 'rxjs/Observable';
+import { ADD_TIMETRACK, addTimetrackSuccess } from './timetrack.action';
+
+export const addTimetrackEpic = action$ => {
+  return action$.ofType(ADD_TIMETRACK).pipe(
+    mergeMap(action => {
+      Observable.fromPromise(
+        fetch('/timetrack', { method: 'post', body: action.payload }),
+      ).map(response => addTimetrackSuccess(response));
+    }),
+  );
+};
