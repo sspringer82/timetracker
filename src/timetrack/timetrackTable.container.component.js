fec41d508340b0eb5839c08d8844e864f50aa95f
Sebastian Springer <info@sebastian-springer.com>
2018-02-01 06:36:52 +0100

    add delete function to container

--- src/timetrack/timetrackTable.container.component.js
+++ src/timetrack/timetrackTable.container.component.js
@@ -1,10 +1,14 @@
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { TimetrackTable } from './timetrackTable.component';
+import { deleteTimetrack } from './timetrack.action';
 
 const mapStateToProps = state => ({
   items: state.timetrack.items,
 });
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  handleDelete: bindActionCreators(deleteTimetrack, dispatch),
+});
 
 export const TimetrackTableContainer = connect(
   mapStateToProps,
