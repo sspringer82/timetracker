d78909e0def8506d69b6bdb20b6008e3490893a7
Sebastian Springer <info@sebastian-springer.com>
2018-03-31 21:16:51 +0200

    cleanup

--- src/timetrack/timetrackTable.component.js
+++ src/timetrack/timetrackTable.component.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-
 import { TimetrackItem } from './timetrackItem.component';
 
 const Table = styled.table`
