394c95b78242fc15aa95f4a11d26be88d6a3f3bc
Sebastian Springer <info@sebastian-springer.com>
2020-09-26 10:07:13 +0200

    restart

--- src/timetrack/timetrack.type.js
+++
@@ -1,8 +0,0 @@
-import PropTypes from 'prop-types';
-
-export const timetrackShape = PropTypes.shape({
-  start: PropTypes.number,
-  end: PropTypes.number,
-  project: PropTypes.string,
-  task: PropTypes.string,
-});
