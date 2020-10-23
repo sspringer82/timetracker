cac01ed2537102f545b70a5136e6469156139a5f
Sebastian Springer <info@sebastian-springer.com>
2020-10-23 20:37:27 +0200

    initial

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import * as _ from 'lodash';
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.appendChild(component());
+})
