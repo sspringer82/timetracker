da90d6ccae85fb2fe5837f930fe813a8f29f9a1e
Sebastian Springer <info@sebastian-springer.com>
2020-10-26 15:55:22 +0100

    readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Timetracker
+
+Simple time tracking application written in TypeScript.
+
+## Installation
+
+Install Dependencies: `npm install`
+
+Run Backend: `npm run backend`
+
+Run Frontend (separate Console): `npm start`
--- src/util/formatDuration.ts
+++ src/util/formatDuration.ts
@@ -1,6 +1,10 @@
-
 export function formatDuration(durationInSeconds: number): string {
   const minutes = Math.abs(Math.floor(durationInSeconds / 60) % 60);
   const hours = Math.abs(Math.floor(durationInSeconds / 3600));
-  return `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}`;
+  if (isNaN(minutes) || isNaN(hours)) {
+    return '00:00';
+  }
+  return `${hours < 10 ? '0' + hours : hours}:${
+    minutes < 10 ? '0' + minutes : minutes
+  }`;
 }
