df59580cf1eb83023d852474fb507233b796b002
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-12-05 09:24:02 +0100

    Form validation

--- src/form/Form.test.tsx
+++ src/form/Form.test.tsx
@@ -1,4 +1,10 @@
-import { act, fireEvent, render, screen } from '@testing-library/react';
+import {
+  act,
+  findByText,
+  fireEvent,
+  render,
+  screen,
+} from '@testing-library/react';
 import Form from './Form';
 
 describe('Form', () => {
@@ -68,5 +74,38 @@ describe('Form', () => {
       end: 1609493400000,
       project: 'schlafen',
     });
+  });
+
+  it('should validate correctly', async () => {
+    const onSave = jest.fn();
+
+    await act(async () => {
+      await render(<Form onSave={onSave} />);
+    });
+
+    await act(async () => {
+      fireEvent.change(screen.getByTestId('form-start'), {
+        target: {
+          value: '2021-01-01T08:30',
+        },
+      });
+      fireEvent.change(screen.getByTestId('form-end'), {
+        target: {
+          value: '2021-01-01T07:30',
+        },
+      });
+    });
+
+    await act(async () => {
+      fireEvent.click(screen.getByTestId('form-submit'));
+    });
+
+    expect(onSave).not.toHaveBeenCalled();
+    expect(
+      await screen.findByText('Ende darf nicht vor Start liegen'),
+    ).toBeInTheDocument();
+    expect(
+      await screen.findByText('Projekt ist ein Pflichtfeld'),
+    ).toBeInTheDocument();
   });
 });
--- src/form/Form.tsx
+++ src/form/Form.tsx
@@ -1,11 +1,14 @@
-import { format } from 'date-fns';
 import {
-  ChangeEvent,
-  FormEvent,
-  ReactElement,
-  useEffect,
-  useState,
-} from 'react';
+  Formik,
+  Form as FormikForm,
+  Field,
+  FormikHelpers,
+  ErrorMessage,
+} from 'formik';
+import * as Yup from 'yup';
+
+import { format } from 'date-fns';
+import { ReactElement } from 'react';
 import { Booking, InputBooking } from '../Booking';
 
 type Props = {
@@ -25,74 +28,76 @@ const initialBooking: FormValues = {
   project: '',
 };
 
+const validationSchema = Yup.object().shape({
+  start: Yup.date().required('Start ist ein Pflichtfeld'),
+  end: Yup.date()
+    .min(Yup.ref('start'), 'Ende darf nicht vor Start liegen')
+    .required('Ende ist ein Pflichtfeld'),
+  project: Yup.string().required('Projekt ist ein Pflichtfeld'),
+});
+
+function getInitialValues(booking?: Booking): FormValues {
+  if (booking) {
+    return {
+      start: format(booking.start, "yyyy-MM-dd'T'HH:mm"),
+      end: format(booking.end, "yyyy-MM-dd'T'HH:mm"),
+      project: booking.project,
+    };
+  }
+  return initialBooking;
+}
+
 const Form = ({ booking, onSave }: Props): ReactElement => {
-  const [formValues, setFormValues] = useState<FormValues>(initialBooking);
-
-  useEffect(() => {
-    if (booking) {
-      setFormValues({
-        start: format(booking.start, "yyyy-MM-dd'T'HH:mm"),
-        end: format(booking.end, "yyyy-MM-dd'T'HH:mm"),
-        project: booking.project,
-      });
-    }
-  }, []);
-
-  function handleSubmit(e: FormEvent) {
-    e.preventDefault();
+  function handleSubmit(
+    values: FormValues,
+    { setSubmitting, resetForm }: FormikHelpers<FormValues>,
+  ) {
     const submittedBooking: InputBooking = {
-      start: new Date(formValues.start).getTime(),
-      end: new Date(formValues.end).getTime(),
-      project: formValues.project,
+      start: new Date(values.start).getTime(),
+      end: new Date(values.end).getTime(),
+      project: values.project,
     };
     if (booking && booking.id) {
       submittedBooking.id = booking.id;
     }
     onSave(submittedBooking);
-    setFormValues(initialBooking);
-  }
-
-  function handleChange(e: ChangeEvent<HTMLInputElement>) {
-    const { name, value } = e.target;
-    setFormValues((prevValues) => ({ ...prevValues, [name]: value }));
+    setSubmitting(false);
+    resetForm();
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="start">
-        Start:
-        <input
-          type="text"
-          name="start"
-          value={formValues.start}
-          onChange={handleChange}
-          data-testid="form-start"
-        />
-      </label>
-      <label htmlFor="end">
-        Ende:
-        <input
-          type="text"
-          name="end"
-          value={formValues.end}
-          onChange={handleChange}
-          data-testid="form-end"
-        />
-      </label>
-      <label htmlFor="project">
-        Projekt:
-        <input
-          type="text"
-          name="project"
-          value={formValues.project}
-          onChange={handleChange}
-          data-testid="form-project"
-        />
-      </label>
-      <button type="submit" data-testid="form-submit">
-        speichern
-      </button>
-    </form>
+    <Formik
+      initialValues={getInitialValues(booking)}
+      onSubmit={handleSubmit}
+      validationSchema={validationSchema}
+    >
+      {({ isSubmitting }) => (
+        <FormikForm>
+          <label htmlFor="start" style={{ display: 'inline-block' }}>
+            Start:
+            <Field type="text" name="start" data-testid="form-start" />
+            <ErrorMessage name="start" component="div" />
+          </label>
+          <label htmlFor="end" style={{ display: 'inline-block' }}>
+            Ende:
+            <Field type="text" name="end" data-testid="form-end" />
+            <ErrorMessage name="end" component="div" />
+          </label>
+          <label htmlFor="project" style={{ display: 'inline-block' }}>
+            Projekt:
+            <Field type="text" name="project" data-testid="form-project" />
+            <ErrorMessage name="project" component="div" />
+          </label>
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            data-testid="form-submit"
+          >
+            speichern
+          </button>
+        </FormikForm>
+      )}
+    </Formik>
   );
 };
 
